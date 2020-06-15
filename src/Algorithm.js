6202f04f11221224af29e0baddddc5543534ce99
lightfire224 <elias.taveras@columbia.edu>
2020-06-15 19:46:45 -0400

    Optimized version

--- src/Algorithm.js
+++ src/Algorithm.js
@@ -3,18 +3,21 @@ import React from 'react'
 export default class Algorithm extends React.Component{
     
     bubbleSort = async (arr) =>{
-        
+        var noSwaps
         for(var i=0; i<arr.length; i++){
+            noSwaps = true
             for (var j = 0; j < arr.length; j++){
                 var shouldSwap = arr[j] > arr[j+1]
                 if(shouldSwap) {
                     let temp = arr[j]
                     arr[j] = arr[j+1]
                     arr[j+1] = temp
+                    noSwaps = false
                 }
                 await this.sleep(250)
                 this.props.sortButton(arr, j, j+1, shouldSwap)
             }
+            if(noSwaps) break;
         }
         return arr
     }
